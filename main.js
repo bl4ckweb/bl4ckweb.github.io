0723657fb2c94137f02379a774a7542c7b2ab62a
Black Web <109763652+bl4ckweb@users.noreply.github.com>
2022-08-31 13:50:53 -0500

    initial commit

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function wsp()
     let nombre = document.getElementById('nombre');
     let email = document.getElementById('email');
     let mensaje = document.getElementById('mensaje');
+    let numero = document.getElementById('numero');
 
-    window.open(`https://api.whatsapp.com/send?phone=51973988036&text=Hola%20mi%20nombre%20es%20${nombre}%20,mi%20correo%20es%20${email}%20y%20${mensaje}`,'_blank')
+    window.open(`https://api.whatsapp.com/send?phone=51973988036&text=Hola%20mi%20nombre%20es%20${nombre}%20,mi%20correo%20es%20${email}%20mi%20teléfono%20es%20${numero}%20y%20${mensaje}`,'_blank')
 }
